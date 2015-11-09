c3c94be7ee0f0d31b7916781e54e9a7f6e5f693f
Braxton Fair <hello@braxtonfair.com>
2015-11-08 21:38:21 -0600

    Add staff middleware

---
+++ app/Http/Middleware/Helper.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace ConnectU\Http\Middleware;
+
+use Auth;
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+class Helper
+{
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param  Guard  $auth
+     * @return void
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        # Checks to see if the current user is staff
+        if (!Auth::user()->isStaff(Auth::user())) {
+            if ($request->ajax()) {
+                return response('Unauthorized.', 401);
+            } else {
+                # Return ack if the user is not staff
+                return redirect()->back()->with('info', 'You are not allowed to view that page!');
+            }
+        }
+
+        return $next($request);
+    }
+}
---
+++ app/Http/Middleware/Moderator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace ConnectU\Http\Middleware;
+
+use Auth;
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+class Moderator
+{
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param  Guard  $auth
+     * @return void
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        # Checks to see if the current user is a moderator or an administrator
+        if (!Auth::user()->isModAndUp(Auth::user())) {
+            if ($request->ajax()) {
+                return response('Unauthorized.', 401);
+            } else {
+                # Return ack if the user is not a moderator or administrator
+                return redirect()->back()->with('info', 'You are not allowed to view that page!');
+            }
+        }
+
+        return $next($request);
+    }
+}

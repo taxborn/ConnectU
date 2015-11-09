c7023bea344035fd877aa7236b0633df79d0aac5
Braxton Fair <hello@braxtonfair.com>
2015-11-08 20:42:52 -0600

    Start work on the User Model

---
+++ app/Models/User.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+
+class User extends Model implements AuthenticatableContract
+{
+    use Authenticatable;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+    * The dates in the project
+    *
+    * @var array
+    */
+
+    protected $dates = ['last_activity']
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'email',
+        'username',
+        'first_name',
+        'last_name',
+        'password',
+        'location',
+        'biography',
+        'gender',
+        'position',
+        'last_activity',
+        'ip',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'ip',
+    ];
+}

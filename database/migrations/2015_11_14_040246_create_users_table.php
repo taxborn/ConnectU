24fc14032f80b880ae75ad66092fd50826a7c620
Braxton Fair <hello@braxtonfair.com>
2015-11-13 22:19:54 -0600

    Add migrations

--- database/migrations/2015_11_14_040246_create_users_table.php
+++ database/migrations/2015_11_14_040246_create_users_table.php
@@ -14,9 +14,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
+            $table->string('email');
+            $table->string('username');
+            $table->string('password');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('location')->nullable();
+            $table->string('biograhpy')->nullable();
+            $table->string('sex');
+            $table->string('position')->nullable();
+            $table->string('ip')->nullable();
+            $table->timestamp('last_activity');
             $table->rememberToken();
             $table->timestamps();
         });
